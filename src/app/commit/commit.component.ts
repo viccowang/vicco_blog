947227beff1e037d65cb834dfbba1f2ccc587b84
Vicco Wang <viccowang@gmail.com>
2017-10-15 17:01:29 +0800

    update commit

--- src/app/commit/commit.component.ts
+++ src/app/commit/commit.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'blog-commit',
-    templateUrl: './commit.component.html'
+    templateUrl: './commit.component.html',
+    styleUrls: ['./commit.component.scss']
 })
 export class BlogCommitComponent {
 
